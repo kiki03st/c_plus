38650382ffe65519bdfa92eb849e1510ce588154
kiki03st <102171184+kiki03st@users.noreply.github.com>
2023-10-11 14:27:04 +0000

    2023.10.11

--- test.cpp
+++ test.cpp
@@ -2,18 +2,19 @@
 #include <string>
 using namespace std;
 
+int result(int n){
+  int num = 1;
+  for(int i = 0; i < n; i++){
+    num*=2;
+  }
+  return num;
+}
 int main()
 {
-  string a;
-  string b;
-
-  a = "This is a string sample!";
-
-  cout << a << endl;
-
-  cout << "Input your string: ";
-  cin >> b;
-
-  cout << "Your string is -- " << b << " --" << endl;
+  int sum = 0;
+  for(int i = 0; i <= 22; i++){
+    sum+=result(i);
+  }
+  cout<<sum<<endl;
   return 0;
 }
