a8cfde0545c16dc77f036de386598daaa2300357
kiki03st <rkdtjddbs0347@gmail.com>
2023-11-15 19:32:55 +0900

    cpp

--- cpp12/2^Reverse linked list.cpp
+++ cpp12/2^Reverse linked list.cpp
@@ -2,43 +2,58 @@
 
 using namespace std;
 
-typedef struct list{
-	int value;
-	list *next;
-}list;
+template <typename T>
+struct list{
+	T value;
+	list<T>* next;
+};
 
-void print(list *n){
-	while(n != NULL){
-		cout << n -> value << endl;
-		n = n -> next;
-	}
+template <typename T>
+list<T>* append(T data){
+	list<T>* p = new(list<T>);
+	p -> value = data;
+	p -> next = NULL;
+	return p;
 }
 
-list* append(int value){
-	list *n = new(list);
-	n -> value = value;
-	n -> next = NULL;
-	return n;
+template <typename T>
+list<T>* append(list<T>* node, T data){
+	list<T>* p = new(list<T>);
+	p -> value = data;
+	p -> next = NULL;
+	node = node -> next = p;
+	return node;
 }
+
+template <typename T>
+list<T>* reverse(list<T>* head){
+	list<T>* loop = new(list<T>);
+	int cnt = 0, num = 0;
+	for(loop = head; loop != NULL; loop = loop -> next) cnt++;
+	int* arr = new int[cnt]();
+	for(loop = head; loop != NULL; loop = loop -> next) arr[num++] = loop -> value;
+	for(loop = head; loop != NULL; loop = loop -> next) loop -> value = arr[--cnt];
+	delete[] arr;
+	return head;
+}
+	
 
 int main(){
-	list *head = NULL;
-	list *loop = NULL;
-	head = append(10);
-	head -> next = append(20);
-	head -> next -> next = append(30);
-	print(head);
-	for(loop = head; loop != NULL; loop = loop -> next){
-		cout << loop -> value << endl;
-	}
-	/*
-	head -> value = 10;
-	head -> next = new(list);
-	
-	head -> next -> value = 20;
-	head -> next -> next = new(list);
-	head -> next -> next -> value = 30;
-	head -> next -> next -> next = NULL;
-	*/
+	list<int> *head;
+	list<int> *loop;
+	list<int> *tail;
+
+	list<int>* r_head;
+
+	tail = head = append(10);
+	tail = append(tail, 20);
+	tail = append(tail, 30);
+	tail = append(tail, 40);
+	for(loop = head; loop != NULL; loop = loop -> next) cout << loop -> value << endl;
+
+	r_head = reverse(head);
+
+	cout << "Reversed " << endl;
+	for(loop = r_head; loop != NULL; loop = loop -> next) cout << loop -> value << endl;
 	return 0;
 }
