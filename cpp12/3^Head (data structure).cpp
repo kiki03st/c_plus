8cbe5892c2e07aae61b94b75b8711f5010e8f275
kiki03st <rkdtjddbs0347@gmail.com>
2023-11-23 06:51:54 +0000

    cpp

--- cpp12/3^Head (data structure).cpp
+++ cpp12/3^Head (data structure).cpp
@@ -2,22 +2,62 @@
 
 using namespace std;
 
-int* sort(int* heap, int index){
+int size = 0;
+int *heap;
 
-
-int* append(int* heap, int len, int data){
-	int* arr = new arr[++len]();
-	for(int i = 0; i < len - 1; i++){
-		arr[i] = heap[i];
+void sort(int index){
+	int i = index + 1;
+	int prev = i / 2;
+	int tmp;
+	if(i == 1) return;
+	else if(heap[i - 1] > heap[prev - 1]){
+		tmp = heap[i - 1];
+		heap[i - 1] = heap[prev - 1];
+		heap[prev - 1] = tmp;
+		sort(prev - 1);
 	}
-	arr[len - 1] = data;
-	return arr;
+}
+
+void append(int data){
+	int* backup = new int[++size];
+	for(int i = 0; i < size - 1; i++) backup[i] = heap[i];
+	backup[size - 1] = data;
+	heap = backup;
+	delete[] backup;
+	sort(size - 1);
+}
+
+int pow(int num){
+	int sum = 1;
+	for(int i = 0; i < num; i++) sum *= 2;
+	return sum;
+}
+
+void print(){
+	int cnt = 0, num = 0;
+	for(int i = 0; i < size; i++){
+		cout << heap[i] << " ";
+		num++;
+		if(num == pow(cnt)){
+			cout << endl;
+			cnt++;
+			num = 0;
+		}
+	}
 }
 
 int main(){
-	int size = 10;
-	int* heap = new int[size]{16, 14, 10, 8, 7, 9, 3, 2, 4, 1};
-	heap = append(heap, size, 15);
-	
+	append(16);
+	append(14);
+	append(10);
+	append(8);
+	append(7);
+	append(9);
+	append(3);
+	append(2);
+	append(4);
+	append(1);
+	append(15);
+	print();
 	return 0;
 }
