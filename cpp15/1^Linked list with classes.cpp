a0305819eef0760398f23206e16cc797e437ded5
kiki03st <rkdtjddbs0347@gmail.com>
2023-11-30 08:33:08 +0000

    cpp

--- cpp15/1^Linked list with classes.cpp
+++ cpp15/1^Linked list with classes.cpp
@@ -1,68 +1,71 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
-class node{
-	public:
-		int data;
-		node *next;
-};
+
+struct node{
+	int data;
+	node *next;
+	int index;
 	
+	node(int d, node *n, int i): data(d), next(n), index(i) {}
+};
+
 class my_list{
 	private:
-		node *stat, *head, *tail;
+		node *head, *current, *start;
+		int index;
 	public:
-		my_list(){
-			head = NULL;
-			stat = tail = head;
-		}
-		my_list& append(int value){
-			if(head == NULL){
-				head = new node;
-				stat = tail = head;
-				tail -> data = value;
-				tail -> next = NULL;
-			}
-			else{
-				node *p = new node;
-				p -> data = value;
-				p -> next = NULL;
-				tail = tail -> next = p;
+		my_list(): head(0), current(0), index(0) {}
+		my_list& append(const int d){
+			head = new node(d, head, index++);
+			if(!current){
+				start = head;
+				current = head;
 			}
 			return *this;
 		}
 		my_list& next(){
-			if(stat -> next == NULL){
-				cout << "더이상 이동할 수 없습니다." << endl;
+			if(index){
+				if(!current){
+					current = start;
+				}
+				current = current -> next;
 			}
-			else{
-				stat = stat -> next;
-			}
+			else cout << "list out of range" << endl;
 			return *this;
 		}
-		int get(){
-			return stat -> data;
-		}
 		void rewind(){
-			stat = head;
-			}
-		void info(){
-			node* loop = new node;
-			int len = 0;
-			int index = 0;
-			for(loop = head; loop != NULL; loop = loop -> next){
-				if(loop == stat) index = len;
-				len++;
-			}
-			cout << "length of list : " << len << endl << "index : " << index << endl;
+			current = start;
 		}
+		int get(){
+			if(!head){
+				cout << "EMPTY : ";
+				return NULL;
+			}
+			else if(!current) current = head;
+			return current -> data;
+		}
+		void info(){
+			cout << "length: " << index << ", current: " << current -> index << endl;
+		}
+/*
+		int operator[](int i){
+			node* s;
+			for(s = head; s -> index != i; s = s -> next);
+			return s -> data;
+		}
+*/
 };
 
 int main(){
 	my_list l;
 	l.append(10).append(20).append(30).append(40);
-	l.next().next();
-	cout << l.get() << endl; // print out 30
-	l.info(); // print out 'length: 4, current: 2 (or 3 depending on your start index)'
+
+	l.next().next().next();
+	cout << l.get() << endl;
+
+	l.info();
 	l.rewind();
 	cout << l.get() << endl;
 	return 0;
