0ea0d8c88c88e115fe4bea89404110bf8b3b13a3
kiki03st <rkdtjddbs0347@gmail.com>
2023-12-04 03:32:10 +0000

    cpp

--- test.cpp
+++ test.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
-#include <ctime>
 
 using namespace std;
-
+class Stack{
+	int stack[16];
+	int sp;
+	void push(int value) { stack[sp++] = value; }
+	int pop() { return stack[--sp]; }
+};
 int main(){
-	char a[16] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00};
-	char *c = (char *)a;
-	int *i = (int *)a;
-	long *l = (long *)a;
-
-	cout << hex;
-	cout << c << endl;
-	cout << *c << endl;
-	cout << (int)*c << endl;
-	cout << i << endl;
-	cout << *i << endl;
-	cout << l << endl;
-	cout << *l << endl;
+	Stack s1;
+	s1.push(10);
+	cout << s1.pop() << endl;
 	return 0;
 }
